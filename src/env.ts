64c91b6a9436a6fb744ddfbab8ec940ed03aabea
erikseki <erikseki@hotmail.com>
2025-02-22 10:15:59 -0300

    implementando o ORM drizzle e criando tabelas do Postgresql

--- src/env.ts
+++ src/env.ts
@@ -2,8 +2,8 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   PORT: z.coerce.number().default(3333),
-  POSTGRES_URL: z.string().url(),
-  REDIS_URL: z.string().url(),
+  POSTGRES_URL: z.string().url(), // url de conexão com o banco de dados
+  REDIS_URL: z.string().url(), // url de conexão com o banco de dados
 })
 
 export const env = envSchema.parse(process.env)

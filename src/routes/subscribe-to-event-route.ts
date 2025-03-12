f4515d954344914a1666db6d7c7198dda343cfcd
erikseki <erikseki@hotmail.com>
2025-03-11 23:17:31 -0300

    commitando o ajuste anterior do controller na rota

--- src/routes/subscribe-to-event-route.ts
+++ src/routes/subscribe-to-event-route.ts
@@ -1,5 +1,6 @@
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import z from 'zod'
+import { subscribeController } from '../controller/subscribe-controller'
 import { subscribeToEvent } from '../functions/subscribe-to-event'
 // cadastrar uma subscriptions de usuários com post
 export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
@@ -23,20 +24,23 @@ export const subscribeToEventRoute: FastifyPluginAsyncZod = async app => {
         },
       },
     },
-    async (request, reply) => {
-      const { name, email, referrer } = request.body
-      2
-      // aqui seria a criação da inscrição no banco de dados
-      const { subscriberId } = await subscribeToEvent({
-        name,
-        email,
-        referrerId: referrer,
-      })
 
-      return reply.status(201).send({
-        subscriberId,
-      })
-      // ao invés do 200, utilizar o 201 sucesso e o recurso foi criado
-    }
+    subscribeController
+
+    // async (request, reply) => {
+    //   const { name, email, referrer } = request.body
+    //   2
+    //   // aqui seria a criação da inscrição no banco de dados
+    //   const { subscriberId } = await subscribeToEvent({
+    //     name,
+    //     email,
+    //     referrerId: referrer,
+    //   })
+
+    //   return reply.status(201).send({
+    //     subscriberId,
+    //   })
+    //   // ao invés do 200, utilizar o 201 sucesso e o recurso foi criado
+    // }
   )
 }
